6803c51111626901acb0a9663520f721760b70aa
bitebait <williamschwaab@protonmail.com>
2022-06-08 16:08:44 -0300

    fix: certificate invalid domain

--- crawler/spiders/bonanzacambios.go
+++ crawler/spiders/bonanzacambios.go
@@ -14,7 +14,7 @@ func BonanzaCambios(channel chan models.Store) {
 	spider.Name = "bonanzacambios"
 	spider.Channel = channel
 	spider.Selector = "div.content-inner:nth-child(1) > div:nth-child(2) > div:nth-child(2) > section:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(5)"
-	spider.URL = "https://bonanzacambios.com.py/"
+	spider.URL = "https://www.bonanzacambios.com.py/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(e.Text)
 		return data
--- crawler/spiders/visaovip.go
+++ crawler/spiders/visaovip.go
@@ -14,7 +14,7 @@ func VisaoVip(channel chan models.Store) {
 	spider.Name = "visaovip"
 	spider.Channel = channel
 	spider.Selector = "div.ui-g-12 div.ui-g-6.painel-cambio-topo:nth-child(1)"
-	spider.URL = "http://www.visaovip.com/"
+	spider.URL = "http://visaovip.com/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(e.Text)
 		return data
