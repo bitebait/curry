e1ecf28dc7009f2be1e4412a1000e0fde9d4ab6c
bitebait <williamschwaab@protonmail.com>
2022-06-08 16:46:11 -0300

    fix: crawler css selector

--- crawler/spiders/shoppingcentropioneer.go
+++ crawler/spiders/shoppingcentropioneer.go
@@ -13,10 +13,10 @@ func ShoppingCentro(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "shoppingcentropioneer"
 	spider.Channel = channel
-	spider.Selector = "div.h-4:nth-child(1)"
+	spider.Selector = ".quotation > li:nth-child(1) > span:nth-child(2)"
 	spider.URL = "https://shoppingcentropioneer.com/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
-		data := strings.TrimSpace(strings.ReplaceAll(e.Text, ".", ""))
+		data := strings.TrimSpace(e.Text)
 		return data
 	}
 
--- crawler/spiders/visaovip.go
+++ crawler/spiders/visaovip.go
@@ -13,7 +13,7 @@ func VisaoVip(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "visaovip"
 	spider.Channel = channel
-	spider.Selector = "div.ui-g-12 div.ui-g-6.painel-cambio-topo:nth-child(1)"
+	spider.Selector = "div.ui-panelgrid-cell:nth-child(1) > span:nth-child(1)"
 	spider.URL = "http://visaovip.com/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(e.Text)
