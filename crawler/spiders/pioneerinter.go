359479f035dc25ae5bccdd676408e6065c6f21ca
bitebait <william@schwaab.me>
2022-11-10 20:57:22 -0300

    fix: pioneerinter spider

--- crawler/spiders/pioneerinter.go
+++ crawler/spiders/pioneerinter.go
@@ -13,7 +13,7 @@ func PioneerInter(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "pioneerinter"
 	spider.Channel = channel
-	spider.Selector = "li.dropDown:nth-child(2) > nav:nth-child(2) > a:nth-child(1)"
+	spider.Selector = ".currency-selector > span:nth-child(1)"
 	spider.URL = "https://www.pioneerinter.com/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(strings.Split(e.Text, "=")[1])
