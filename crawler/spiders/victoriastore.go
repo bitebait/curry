4f286da23105e6a31b23cd28d49e3bc6b34f38d3
bitebait <william@schwaab.me>
2022-11-10 20:06:02 -0300

    fix: victoriastore spider css selector

--- crawler/spiders/victoriastore.go
+++ crawler/spiders/victoriastore.go
@@ -13,10 +13,10 @@ func VictoriaStore(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "victoriastore"
 	spider.Channel = channel
-	spider.Selector = "div.currency-selector"
+	spider.Selector = ".left-content > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > div:nth-child(1) > span:nth-child(1)"
 	spider.URL = "https://www.victoriastore.com.br/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
-		data := strings.TrimSpace(strings.Split(e.ChildText("span"), "=")[1])
+		data := strings.TrimSpace(strings.Split(e.Text, "=")[1])
 		return data
 	}
 
