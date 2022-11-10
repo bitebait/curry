b08a604b4d07b63709dc8c492f501118a750510d
bitebait <william@schwaab.me>
2022-11-10 20:46:54 -0300

    fix: madridcenter spider

--- crawler/spiders/madridcenter.go
+++ crawler/spiders/madridcenter.go
@@ -13,7 +13,7 @@ func MadridCenter(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "madridcenter"
 	spider.Channel = channel
-	spider.Selector = "body > header:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(3) > span:nth-child(1) > span:nth-child(1) > strong:nth-child(2)"
+	spider.Selector = "body > header > div > div > div > div.item.top-quotes > div.col-12.text3 > span > span:nth-child(1) > strong"
 	spider.URL = "https://www.madridcenter.com/"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(e.Text)
