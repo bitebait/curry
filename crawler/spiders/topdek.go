d7381546147bb8edfc789efe18e5c7ee1514be5d
bitebait <william@schwaab.me>
2022-11-10 21:14:26 -0300

    fix: topdek spider

--- crawler/spiders/topdek.go
+++ crawler/spiders/topdek.go
@@ -13,7 +13,7 @@ func TopDek(channel chan models.Store) {
 	spider := &config.Spider{}
 	spider.Name = "topdek"
 	spider.Channel = channel
-	spider.Selector = "body > div:nth-child(9) > div:nth-child(1) > p:nth-child(1)"
+	spider.Selector = "body > div:nth-child(8) > div:nth-child(1) > p:nth-child(1)"
 	spider.URL = "https://www.topdek.com.br/br"
 	spider.GetValue = func(e *colly.HTMLElement) string {
 		data := strings.TrimSpace(strings.Split(e.Text, " ")[3])
