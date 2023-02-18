7fb5367733d57c1c49441023ea0b7f11b7f553ed
bitebait <william@schwaab.me>
2023-02-18 16:46:20 -0300

    fix: hbgames spider

--- crawler/spiders/hbgames.go
+++ crawler/spiders/hbgames.go
@@ -3,7 +3,7 @@ package spiders
 func init() {
 	NewSpider(
 		"hbgames",
-		"body > div.main.document > header.header.site.show-for-medium > div.bar.top > div > div.first.row.grid-x > div > div > div.quotation.cell.small-30.medium-25.large-12 > div.value > span:nth-child(2)",
+		".arial > div:nth-child(2) > b:nth-child(1)",
 		"http://www.hbgamespy.com/",
 	)
 }
