a55f0d56daba9114d899c66094d2bf6d3eda71ab
bitebait <william@schwaab.me>
2023-09-15 21:53:34 +0200

    fix: icompy spider

--- crawler/spiders/icompy.go
+++ crawler/spiders/icompy.go
@@ -3,7 +3,7 @@ package spiders
 func init() {
 	NewSpider(
 		"icompy",
-		"div.pt_menu:nth-child(2) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1)",
+		"#header > div > div > div > section.elementor-element.elementor-element-cldqzgw.elementor-section-stretched.elementor-section-boxed.elementor-section-height-default.elementor-section-content-middle.sticky-inner.elementor-hidden-tablet.elementor-hidden-phone.elementor-section.elementor-top-section > div > div > div.elementor-element.elementor-element-ligyjdn.elementor-column.elementor-col-25.elementor-top-column > div > div > div > div > div > div.elementor-icon-box-content > div",
 		"http://icompy.com/",
 	)
 }
